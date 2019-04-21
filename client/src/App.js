80b1ac10161da4cb48b2b5463a8491d2fe87c663
pickupst <gd.okanuslu@gmail.com>
2019-04-21 09:52:17 +0300

    Creating a map

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 
 const CANVAS_WIDTH = 640;
-const CANVAS_HEIGHT = 320;
+const CANVAS_HEIGHT = 512;
+const TILE_WIDTH = 64;
+const TILE_HEIGHT = 64;
+
 
 const Player = {
 
@@ -28,52 +31,150 @@ const Player = {
 
 };
 
-const Game = {
+class Game {
 
-  users : [],
+  images = {};
 
-  load: async () => {
+  layers = [
+    [
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    ]
+
+  ];
+
+  users = [];
+
+  
+  constructor(ctx) {
+    //Oyunu başlatacak fonksiyon
+    this.ctx = ctx;
+    }
+
+  init = async () => {
     //Resimler yüklenecek
 
-  },
+    const tile0 = await this.loadImage('./assets/layers/0.png');
+    const tile1 = await this.loadImage('./assets/layers/1.png');
 
-  init: () => {
-    //Oyunu başlatacak fonksiyon
+    this.images = {
+      0: tile0,
+      1: tile1,
+    }
 
-  },
 
-  update: () => {
+  };
+
+  update = () => {
     //Oyunun döngüsü burada olacak
 
-  },
 
-  draw: () => {
+  };
+
+  draw = () => {
     //Oyunu ekrana çizdirecek fonksiyon
 
-  }
+    this.ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //this.users.forEach(user => user.draw());
+  
+    const cols = CANVAS_WIDTH / TILE_WIDTH;
+    const rows = CANVAS_HEIGHT / TILE_HEIGHT;
 
-};
+    for (var i = 0; i < this.layers.length; i++) {
+      const layer = this.layers[i];
+      for (var j = 0; j < rows; j++) {
+        for (var k = 0; k < cols; k++) {
+          const imageType = layer[j * cols + k];
+
+          console.log("imageType: " + layer[j * cols + k]);
+          this.ctx.drawImage(
+            this.images[imageType],
+            0, 0, TILE_WIDTH, TILE_HEIGHT, 
+            k * TILE_WIDTH, j * TILE_HEIGHT, 
+            TILE_WIDTH, TILE_HEIGHT);
+    
+        }
+      }
+    }
+    
+  };
+
+  loadImage = (src) => {
+    var img = new Image();
+      var d = new Promise(function (resolve, reject) {
+        img.onload = function () {
+          resolve(img);
+        }.bind(this);
+  
+        img.onerror = function () {
+          reject('hata ' + src);
+        };
+      }.bind(this));
+  
+      img.src = src;
+      return d;
+  };
+
+}
 
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.canvasRef = React.createRef();
+    
     this.state = {
 
       CURRENT_STEP: '',
+      isGameRunning: false,
 
 
     };
 
+    this.canvasRef = React.createRef();
+    this.lastLoop = null;
+  }
+
+  start = async () => {
+    
+    if (!this.state.isGameRunning) {
+  
+    this.game = new Game(this.getCtx());
+    await this.game.init();
+    this.loop();
+    }
+    this.setState(state =>  ({isGameRunning: state.isGameRunning}));
+  }
+
+  loop = () => {
+    requestAnimationFrame(() => {
+      const now = Date.now();
+      if (now - this.lastLoop > (1000 / 30)) {
+        this.game.update();
+      }
+
+      this.game.draw();
+
+      this.lastLoop = Date.now();
+      if (this.state.isGameRunning) {
+        this.loop();
+      }
+
+    });
   }
 
   getCtx = () => this.canvasRef.current.getContext('2d');
 
   render() {
     return (
-      <div>
-        hello world!
+      <div style = {{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black'}}>
+        <button onClick ={this.start}> START! </button>
+
         <canvas ref = {this.canvasRef} width = {CANVAS_WIDTH} height = {CANVAS_HEIGHT}>
 
         </canvas>
